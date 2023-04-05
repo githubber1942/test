30118e38d3cd2edd4bf38c787caf9919f0a38b19
githubber1942 <waltondyer43@gmail.com>
2023-04-05 14:55:46 +0100

    Checking api key

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::post('/save-api-key', function (Illuminate\Http\Request $request) {
+    $apiKey = $request->input('api_key');
+
+    $response = Http::withHeaders([
+        'Authorization' => 'Bearer ' . $apiKey,
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+    ])->get('https://connect.mailerlite.com/api/subscribers', []);
+
+    $msg = $response->json();
+    $stat = $response->status();
+
+    if ($response->status() === 401) {
+        $errorMsg = isset($msg['message']) ? $msg['message'] : 'No response message';
+        return redirect('/home')->with('error', 'Invalid API key! ' . $errorMsg . ' with status ' . $stat);
+        // return redirect('/home')->with('error', 'Invalid API key! ' . json_encode($msg.message));
+    }
+
+    if ($response->failed() || $response->status() !== 200) {
+        return redirect('/home')->with('error', 'Invalid API key! ' . json_encode($msg) . ' with status ' . $stat);
+    }
+    
+    return redirect('/home')->with('success', 'API key checks out! ' . json_encode($msg) . ' with status ' . $stat);
+})->name('save-api-key');
